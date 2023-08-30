c81e65d350e5728c3c5e60152fd2107c821f04b3
Paulinxxz <frichepaulinho@gmail.com>
2023-08-30 09:21:34 -0300

    Status Funcionando

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import { useEffect, useState } from "react";
 function App() {
 
   const [ listatarefas, setListaTarefas ] = useState( [] );
-  const [ tarefa, setTarefa ] = useState( { id: '' , texto: "" } );
+  const [ tarefa, setTarefa ] = useState( { id: '' , texto: "" , status: "" } );
 
   function AddTarefa()
   {
@@ -14,12 +14,19 @@ function App() {
   
   function excluirTarefa( id )
   {
-    const novaLista = listatarefas.filter( ( tarefa ) => tarefa.id !== id);
+    const novaLista = listatarefas.filter( ( tarefa ) => tarefa.id !== id );
     setListaTarefas( novaLista );
   }
 
+  function statusTarefa( id, status )
+  {
+    const index = listatarefas.findIndex( ( tarefa ) => tarefa.id === id );
+    listatarefas[index].status = !status;
+    setListaTarefas( [...listatarefas ] );
+  }
+
   useEffect( () => {
-    setTarefa( { id: "" , texto: "" } );
+    setTarefa( { id: "" , texto: "" , status: "" } );
   }, [ listatarefas ] )
 
   return (
@@ -28,13 +35,13 @@ function App() {
           <h1>React DO</h1>
       </header>
       <div>
-          <input type="text" name="tarefa" placeholder="Digite sua tarefa" value={tarefa.texto} onChange={ (e) => setTarefa( { id: Math.random(), texto: e.target.value } ) }/>
+          <input type="text" name="tarefa" placeholder="Digite sua tarefa" value={tarefa.texto} onChange={ (e) => setTarefa( { id: Math.random(), texto: e.target.value, status: false } ) }/>
           <button onClick={AddTarefa}>Adicionar</button>
       </div>
       <div>
         <ul>
           {listatarefas.map( (item, index ) => (
-            <li key={index}>{item.texto} <button onClick={ () => excluirTarefa(item.id) } >Excluir</button></li>
+            <li key={index}>{item.texto} <button onClick={ () => statusTarefa(item.id, item.status) }>{item.status ? 'Concluída' : 'Não Concluída' }</button><button onClick={ () => excluirTarefa(item.id) }>Excluir</button></li>
           ))}
         </ul>
       </div>
